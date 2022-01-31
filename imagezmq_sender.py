b34dd9779c3e983bc52e2323920f7b3134e0ad3d
dek <dakoner@gmail.com>
2022-01-30 16:03:37 -0800

    broadcast

--- imagezmq_sender.py
+++ imagezmq_sender.py
@@ -4,17 +4,18 @@ from simple_pyspin import Camera
 import cv2
 import numpy as np
 
-HUB='tcp://desktop-h3tsld0.local:5555'
+#HUB='tcp://gork.local:5555'
+port = 5555
 
-# if __name__ == '__main__':
-#     cam = Camera()
-#     cam.init()
-#     sender = imagezmq.ImageSender(connect_to=HUB)
-#     cam.start()
-#     while True:
-#         img = cam.get_array()
-#         jpg_buffer = simplejpeg.encode_jpeg(img[..., np.newaxis], quality=55, colorspace='GRAY')
-#         sender.send_jpg("microscope", jpg_buffer)
+if __name__ == '__main__':
+    cam = Camera()
+    cam.init()
+    sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
+    cam.start()
+    while True:
+        img = cam.get_array()
+        jpg_buffer = simplejpeg.encode_jpeg(img[..., np.newaxis], quality=55, colorspace='GRAY')
+        sender.send_jpg("microscope", jpg_buffer)
 
 # if __name__ == '__main__':
 #     cap = cv2.VideoCapture(0)
